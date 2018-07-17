cf0931ba7ea1b3c47dbe8bb1b7ae7ba64c5a4f58
Claudio Escobar <info@minimsoftware.com>
2018-07-17 15:36:39 -0300

    Session error on Check Exp

--- check_exp.php
+++ check_exp.php
@@ -20,10 +20,11 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
+session_start();
 
 include('odm-load.php');
 $start_time = time();
-session_start();
+
 
 $last_message = (isset($_REQUEST['last_message']) ? $_REQUEST['last_message'] : '');
 
